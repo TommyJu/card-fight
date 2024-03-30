b96c791fddd41c7992bfc21a391017be87dfd5e1
Tommy Ju <tommyju25@gmail.com>
2024-03-29 23:43:10 -0700

    Organize code

--- src/main/java/ca/bcit/comp2522/termproject/cards/MainApplication.java
+++ src/main/java/ca/bcit/comp2522/termproject/cards/MainApplication.java
@@ -1,43 +1,18 @@
 package ca.bcit.comp2522.termproject.cards;
-
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
-
-import java.util.List;
-
 
 public class MainApplication extends Application {
     private static final int SCREEN_HEIGHT = 800;
     private static final int SCREEN_WIDTH = 800;
     @Override
     public void start(final Stage primaryStage) {
-        primaryStage.setTitle("Elemental Warfare");
-
-//        ImageView backgroundImage = new ImageView("background.jpg");
-//        backgroundImage.setViewport(new Rectangle2D(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
-
-        Image backgroundImage = new Image("background.jpg");
-        BackgroundImage background = new BackgroundImage(backgroundImage,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundRepeat.NO_REPEAT,
-                BackgroundPosition.DEFAULT,
-                new BackgroundSize(1.0, 1.0, true, true, false, false));
-//        background.setViewport(new Rectangle2D(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT));
-
-
         Deck deck = new Deck();
 
         Image image = new Image(deck.getHand().getFirst().getImage(), 128, 179, true, true);
@@ -48,16 +23,17 @@ public class MainApplication extends Application {
             imageView.setImage(new Image(deck.getHand().getFirst().getImage(), 128, 179, true, true));
         });
 
-//        StackPane pane = new StackPane(imageView);
-//
-//        Scene scene = new Scene(pane, SCREEN_WIDTH, SCREEN_HEIGHT);
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-
         // Setting the stage
+        Image backgroundImage = new Image("background.jpg");
+        BackgroundImage background = new BackgroundImage(backgroundImage,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(1.0, 1.0, true, true, false, false));
         Pane root = new Pane(imageView);
         root.setBackground(new Background(background));
         Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT, Color.WHITE);
+        primaryStage.setTitle("Elemental Warfare");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
