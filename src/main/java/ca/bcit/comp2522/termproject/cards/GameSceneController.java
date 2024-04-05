52945577f84bb57106b8fc7c047827f38b78ba5f
Tommy Ju <tommyju25@gmail.com>
2024-04-05 14:17:30 -0700

    Create card on click events

--- src/main/java/ca/bcit/comp2522/termproject/cards/GameSceneController.java
+++ src/main/java/ca/bcit/comp2522/termproject/cards/GameSceneController.java
@@ -11,11 +11,19 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GameSceneController {
     private static Stage stage;
     private static Scene scene;
     private static Game game;
+    private static Player player1;
+    private static Deck player1Deck;
+    private static List<Card> player1Hand;
+    @FXML
+    private static List<ImageView> allCardsInHand;
 
     @FXML
     private ImageView firstCardInHand, secondCardInHand, thirdCardInHand, fourthCardInHand, fifthCardInHand;
@@ -24,6 +32,14 @@ public class GameSceneController {
     public void initialize() {
         // A game is created with the player initialized on the start screen
         game = new Game(StartSceneController.player1);
+
+        player1 = game.getPlayer1();
+        player1Deck = player1.getDeck();
+        player1Hand = player1Deck.getHand();
+
+        allCardsInHand = new ArrayList<>();
+        allCardsInHand.addAll(
+                Arrays.asList(firstCardInHand, secondCardInHand, thirdCardInHand, fourthCardInHand, fifthCardInHand));
         updateCardImages();
     }
 
@@ -36,15 +52,42 @@ public class GameSceneController {
     }
 
     public void updateCardImages() {
-        firstCardInHand.setImage(game.getPlayer1().getDeck().getHand().get(0).getImage());
-        secondCardInHand.setImage(game.getPlayer1().getDeck().getHand().get(1).getImage());
-        thirdCardInHand.setImage(game.getPlayer1().getDeck().getHand().get(2).getImage());
-        fourthCardInHand.setImage(game.getPlayer1().getDeck().getHand().get(3).getImage());
-        fifthCardInHand.setImage(game.getPlayer1().getDeck().getHand().get(4).getImage());
+        int counter = 0;
+        for (ImageView card : allCardsInHand) {
+            card.setImage(player1Hand.get(counter).getImage());
+            counter++;
+        }
     }
 
-    public void startGame() {
-        // game logic here
+    // Card on click methods
+    public void firstCardOnClick() {
+        Card selectedCard = player1Hand.get(0);
+        Card newCard = player1Deck.dealNewCard(selectedCard);
+        firstCardInHand.setImage(newCard.getImage());
+    }
+
+    public void secondCardOnClick() {
+        Card selectedCard = player1Hand.get(1);
+        Card newCard = player1Deck.dealNewCard(selectedCard);
+        secondCardInHand.setImage(newCard.getImage());
+    }
+
+    public void thirdCardOnClick() {
+        Card selectedCard = player1Hand.get(2);
+        Card newCard = player1Deck.dealNewCard(selectedCard);
+        thirdCardInHand.setImage(newCard.getImage());
+    }
+
+    public void fourthCardOnClick() {
+        Card selectedCard = player1Hand.get(3);
+        Card newCard = player1Deck.dealNewCard(selectedCard);
+        fourthCardInHand.setImage(newCard.getImage());
+    }
+
+    public void fifthCardOnClick() {
+        Card selectedCard = player1Hand.get(4);
+        Card newCard = player1Deck.dealNewCard(selectedCard);
+        fifthCardInHand.setImage(newCard.getImage());
     }
 
 }
