83c66af4964832e103f8e5acd2ee4cffd65ed569
Tommy Ju <tommyju25@gmail.com>
2024-04-05 23:24:15 -0700

    Add sailboat :)

--- src/main/java/ca/bcit/comp2522/termproject/cards/StartSceneController.java
+++ src/main/java/ca/bcit/comp2522/termproject/cards/StartSceneController.java
@@ -1,12 +1,15 @@
 package ca.bcit.comp2522.termproject.cards;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -14,12 +17,14 @@ public class StartSceneController {
     public static Stage stage;
     private static Scene scene;
     public static Player player1;
-
+    @FXML
+    private ImageView sailBoat;
 
     // This method is automatically called by the FXML loader
     public void initialize() {
         Deck deck = new Deck();
         player1 = new HumanPlayer("Chris", deck);
+        playSailBoatAnimation();
     }
 
     public void switchToGameScene(ActionEvent event) throws IOException {
@@ -28,5 +33,14 @@ public class StartSceneController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void playSailBoatAnimation() {
+        TranslateTransition translate = new TranslateTransition();
+        translate.setNode(sailBoat);
+        translate.setDuration(Duration.minutes(1));
+        translate.setCycleCount(TranslateTransition.INDEFINITE);
+        translate.setByX(1300);
+        translate.play();
     }
 }
