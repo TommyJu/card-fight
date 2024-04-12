d1632d099163cc28d2c6d0a1a85e3b29ecaaa959
Tommy Ju <tommyju25@gmail.com>
2024-04-12 13:42:42 -0700

    Add FXML injections

--- src/main/java/ca/bcit/comp2522/termproject/cards/DeckBuildSceneController.java
+++ src/main/java/ca/bcit/comp2522/termproject/cards/DeckBuildSceneController.java
@@ -4,19 +4,58 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DeckBuildSceneController {
     private static Scene scene;
     private static List<Card> newDeck;
+    private static int currentCardIndex;
+    @FXML
+    Button saveButton, backButton, previousCard, nextCard;
+    @FXML
+    ImageView selectFireCard, selectWaterCard, selectGrassCard;
+    @FXML
+    Slider attackSlider;
+    @FXML
+    Label currentTotalAttack;
+    @FXML
+    ImageView cardPreview;
+
+    List<ImageView> allCardIcons;
+    List<Label> allCardAttacks;
+    @FXML
+    ImageView cardIcon1, cardIcon2, cardIcon3, cardIcon4, cardIcon5, cardIcon6, cardIcon7, cardIcon8, cardIcon9,
+            cardIcon10, cardIcon11, cardIcon12, cardIcon13, cardIcon14, cardIcon15, cardIcon16, cardIcon17,
+            cardIcon18, cardIcon19, cardIcon20;
+    @FXML
+    Label cardAttack1, cardAttack2, cardAttack3, cardAttack4, cardAttack5, cardAttack6, cardAttack7, cardAttack8,
+            cardAttack9, cardAttack10, cardAttack11, cardAttack12, cardAttack13, cardAttack14, cardAttack15,
+            cardAttack16, cardAttack17, cardAttack18, cardAttack19, cardAttack20;
+
     @FXML
     public void initialize() {
         List<Card> newDeck = MainApplication.player1.getDeck().getReserve();
         List<Card> PlayerHand = MainApplication.player1.getDeck().getHand();
         newDeck.addAll(PlayerHand);
+        currentCardIndex = 0;
+        allCardIcons = new ArrayList<>();
+        allCardIcons.addAll(Arrays.asList(
+                cardIcon1, cardIcon2, cardIcon3, cardIcon4, cardIcon5, cardIcon6, cardIcon7, cardIcon8, cardIcon9,
+                cardIcon10, cardIcon11, cardIcon12, cardIcon13, cardIcon14, cardIcon15, cardIcon16, cardIcon17,
+                cardIcon18, cardIcon19, cardIcon20));
+        allCardAttacks = new ArrayList<>();
+        allCardAttacks.addAll(Arrays.asList(
+                cardAttack1, cardAttack2, cardAttack3, cardAttack4, cardAttack5, cardAttack6, cardAttack7, cardAttack8,
+                cardAttack9, cardAttack10, cardAttack11, cardAttack12, cardAttack13, cardAttack14, cardAttack15,
+                cardAttack16, cardAttack17, cardAttack18, cardAttack19, cardAttack20));
     }
     public void switchToStartScene() throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("StartScene.fxml"));
